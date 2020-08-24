99c75f8b32585b02e5da9c99f3c44ec93b5bf943
AdamCsordas21 <adamcsordas@hotmail.com>
2020-08-24 20:13:33 +0100

    add tsc to the project

---
+++ src/hello.ts
@@ -0,0 +1,15 @@
+interface Person {
+  name: string
+}
+
+const adam = {
+  name: 'Adam Was Here',
+  firstName: 'Adam',
+  lastName: 'Was Here'
+}
+
+function sayHi(person: Person): void {
+  console.log(`How you doing ${person.name}`)
+}
+
+sayHi(adam)
