9d2932d99cc4130d1c2a39af9f777a891c8ae30c
AdamCsordas21 <adamcsordas@hotmail.com>
2020-08-31 02:18:18 +0100

    add Class Types example

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -118,4 +118,37 @@ interface ReadOnlyStringArray {
   readonly [index: number]: string
 }
 
-let myArray2: ReadOnlyStringArray = ['Alice', 'Bib']
+let myArray2: ReadOnlyStringArray = ['Alice', 'Bob']
+
+interface ClockConstructor {
+  new (hour: number, minute: number): ClockInterface
+}
+
+interface ClockInterface {
+  tick(): void
+}
+
+function createClock(
+  ctor: ClockConstructor,
+  hour: number,
+  minute: number
+): ClockInterface {
+  return new ctor(hour, minute)
+}
+
+class DigitalClock implements ClockInterface {
+  constructor(h: number, m: number) {}
+  tick() {
+    console.log('beep beep')
+  }
+}
+
+class AnalogClock implements ClockInterface {
+  constructor(h: number, m: number) {}
+  tick() {
+    console.log('tick tock')
+  }
+}
+
+let digital = createClock(DigitalClock, 12, 17)
+let analog = createClock(AnalogClock, 7, 32)
