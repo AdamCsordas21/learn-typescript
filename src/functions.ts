53d0b46cc08840f56002699f1c22c394ea13bb95
AdamCsordas21 <adamcsordas@hotmail.com>
2020-08-31 14:41:55 +0100

    add this and arrow functions example

--- src/functions.ts
+++ src/functions.ts
@@ -76,3 +76,21 @@ function buildName6(firstName: string, ...restOfName: string[]) {
 }
 
 let buildNameFun: (fname: string, ...rest:string[]) => string
+
+let deck = {
+  suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+  cards: Array(52),
+  createCardPicker: function () {
+    return () => {
+      let pickedCard = Math.floor(Math.random() * 52)
+      let pickedSuit = Math.floor(pickedCard / 13)
+      
+      return { suit: this.suits[pickedSuit], card: pickedCard % 13 }
+    }
+  }
+}
+
+let cardPicker = deck.createCardPicker()
+let pickedCard = cardPicker()
+
+alert('card: ' + pickedCard.card + ' of ' + pickedCard.suit)
