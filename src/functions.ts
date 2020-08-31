5a25e88d479cfb818af929cb32dc4efaa3a9547b
AdamCsordas21 <adamcsordas@hotmail.com>
2020-08-31 03:04:43 +0100

    add Writing the function example

--- src/functions.ts
+++ src/functions.ts
@@ -5,3 +5,17 @@ function add2(x: number, y: number): number {
 let myAdd = function (x: number, y: number): number {
   return x + y
 }
+
+let myAdd2: (x: number, y: number) => number = function (
+  x: number,
+  y: number
+): number {
+  return x + y
+}
+
+let myAdd3: (baseValeu: number, increment: number) => number = function (
+  x: number
+  y: number
+): number {
+  return x + y
+}
