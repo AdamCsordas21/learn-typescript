ffe9fb4b73412d90f2d0e9d005be584680f7d99c
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-03 14:13:36 +0100

    add Classes example

---
+++ classes.ts
@@ -0,0 +1,18 @@
+// Classes
+class Greeter {
+  greeting: string
+  
+  constructor(message: string) {
+    this.greeting = message
+  }
+  
+  greet() {
+    return 'Hello, ' + this.greeting
+  }
+}
+
+let greeter = new Greeter('world')
+
+
+
+export { }
