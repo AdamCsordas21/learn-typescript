e9707da30eb9a4012f69c2002c723da649785333
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-03 04:06:36 +0100

    add unions-and-intersection-types.ts & example

---
+++ src/unions-and-intersection-types.ts
@@ -0,0 +1,23 @@
+// It's a tool that let you compose or combine exosting types instead of creating them from scratch
+
+// Union Types
+function padLeft(value: string, padding: any) {
+  if (typeof padding === 'number') {
+    return Array(padding + 1).join(' ') + value
+  }
+  if (typeof padding === 'string') {
+    return padding + value
+  }
+  throw new Error(`Expected string or number, got '${padding}'.`)
+}
+
+padLeft('Hello world', 4) // returns '    Hello world'
+
+// passes at compile time, fails at runtime
+let indentedString = padLeft('Hello world', true)
+
+function padLeft2(value: string, padding: string | number) {
+  // ...
+}
+
+export { }
