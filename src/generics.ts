98f8321c38211afc63016e5b15d6caf2db674160
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-05 00:00:25 +0100

    add Working with Generic Type Variables example

--- src/generics.ts
+++ src/generics.ts
@@ -18,4 +18,20 @@ let output = indentity<string>('myString')
 let output2 = indentity('myString')
 //         = let output: string
 
+// Working with Generic Type Variables
+function indentity3<T>(arg: T) {
+  // console.log(arg.length) --> Property 'length' does not exist on type 'T'.ts(2339)
+  return arg
+}
+
+function loggingIndetity<T>(arg: T[]) {
+  console.log(arg.length)
+  return arg
+}
+
+function logginIndentity2<T>(arg: Array<T>): Array<T> {
+  console.log(arg.length)
+  return arg
+}
+
 export { }
