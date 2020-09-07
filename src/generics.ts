109514dcfd2ed5bec6f340bafcf3cc622192e7bd
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-07 19:42:49 +0100

    refactor Generics.ts
    Co-authored-by: Osh Slomczynski <karol.slomczynski@gmail.com>

--- src/generics.ts
+++ src/generics.ts
@@ -1,6 +1,6 @@
 // Generics
 // Hello World of Generics
-function identity(arg: number): number {
+function identity1(arg: number): number {
   return arg
 }
 
@@ -8,81 +8,76 @@ function identity2(arg: any): any {
   return arg
 }
 
-function indentity<T>(arg: T): T {
+function identity3<T>(arg: T): T {
   return arg
 }
 
-let output = indentity<string>('myString')
+let output = identity3<string>('myString')
 //       ^ = let output: string
 
-let output2 = indentity('myString')
-//         = let output: string
+let output2 = identity3('myString')
+//         = let output2: string
+
+let output3 = identity3(234)
+//         = let output3: number
 
 // Working with Generic Type Variables
-function indentity3<T>(arg: T) {
+function identity4<T>(arg: T) {
   // console.log(arg.length) --> Property 'length' does not exist on type 'T'.ts(2339)
   return arg
 }
 
-function loggingIndetity<T>(arg: T[]) {
+function loggingIdentity<T>(arg: T[]): T[] {
   console.log(arg.length)
   return arg
 }
 
-function logginIndentity2<T>(arg: Array<T>): Array<T> {
+function loggingIdentity2<T>(arg: Array<T>): Array<T> {
   console.log(arg.length) // Array has a .length, so no more error
   return arg
 }
 
 // Generic Types
-function identity4<T>(arg: T): T {
+function identity5<T>(arg: T): T {
   return arg;
 }
 
-// let myIdentity: <T>(arg: T) => T = identity;
-// Type '(arg: number) => number' is not assignable to type '<T>(arg: T) => T'.
-// Types of parameters 'arg' and 'arg' are incompatible.
-// Type 'T' is not assignable to type 'number'.ts(2322)
+let myIdentity: <T>(arg: T) => T = identity5
 
-function indentity5<T>(arg: T): T {
-  return arg
-}
+let myIdentity2: <U>(arg: U) => U = identity5
 
-// let myIdentity: <U>(arg: U) => U = identity
-// Type '(arg: number) => number' is not assignable to type '<T>(arg: T) => T'.
-// Types of parameters 'arg' and 'arg' are incompatible.
-// Type 'T' is not assignable to type 'number'.ts(2322)
-
-function indentity6<T>(arg: T): T {
-  return arg  
-}
-
-let myIdentity: { <T>(arg: T): T } = indentity
+let myIdentity3: { <T>(arg: T): T } = identity5
 
 interface GenericIdentityFn {
   <T>(arg: T): T
 }
 
-function identity7<T>(arg: T): T {
-  return arg
-}
-
-let myIdentity2: GenericIdentityFn = indentity
+let myIdentity4: GenericIdentityFn = identity5
 
 interface GenericIdentityFn2<T> {
   (arg: T): T
 }
 
-function identity8<T>(arg: T): T {
-  return arg
-}
-
-let myIdentity3: GenericIdentityFn2<number> = identity
+let myIdentity5: GenericIdentityFn2<number> = identity5
+let myIdentity6: GenericIdentityFn2<number> = identity1
 
 // Generic Classes
 class GenericNumber<T> {
   zeroValue: T
   add: (x: T, y: T) => T
+
+  // The above is evil, because it allows you to get a null pointer exception
+  // to mitigate, either make the props optional
+
+  // zeroValue?: T
+  // add?: (x: T, y: T) => T
+
+  // or (better) add a constructor that will require the initial values
+
+  // constructor(zeroValue: T, add: (x: T, y: T) => T) {
+  //   this.zeroValue = zeroValue
+  //   this.add = add
+  // }
 }
 
 let myGenericNumber = new GenericNumber<number>()
@@ -96,9 +91,8 @@ class GenericNumber2<T> {
   zeroValue: T
   add: (x: T, y: T) => T
 }
-
 // ---cut---
-let stringNumber = new GenericNumber<string>()
+let stringNumber = new GenericNumber2<string>()
 stringNumber.zeroValue = ''
 stringNumber.add = function(x, y) {
   return x + y
@@ -122,13 +116,15 @@ function loggingIndentity3<T extends Lengthwise>(arg: T): T {
   return arg
 }
 
+loggingIndentity3('sdfgs')
+loggingIndentity3([1, 2])
+loggingIndentity3({ length: 10, value: 3 })
 // loggingIndentity3(3)
 // Argument of type '3' is not assignable to parameter of type 'Lengthwise'.ts(2345)
 
-loggingIndentity3({ length: 10, value: 3 })
 
 // Using Type Parameters in Generic Constrains
-function getProperty<T , K extends keyof T>(obj: T, key: K) {
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
   return obj[key]
 }
 
@@ -167,7 +163,9 @@ function createInstance<A extends Animal>(c: new () => A): A {
   return new c()
 }
 
+new Lion().keeper.nametag
 createInstance(Lion).keeper.nametag
+new Bee().keeper.hasMask
 createInstance(Bee).keeper.hasMask
 
 export { }
