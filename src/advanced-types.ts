9b4adbaf5dd0da244557901814b163eda4543217
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-10 18:04:21 +0100

    add typeof Type Guards example

--- src/advanced-types.ts
+++ src/advanced-types.ts
@@ -56,4 +56,33 @@ function move(pet4: Fish | Bird) {
   return pet4.fly
 }
 
+// typeof Type Guards
+function isNumber(x: any): x is number {
+  return typeof x === 'number'
+}
+
+function isString(x: any): x is string {
+  return typeof x === 'string'
+}
+
+function padLeft(value: string, padding: string | number) {
+  if (isNumber(padding)) {
+    return Array(padding + 1).join(' ') + value
+  }
+  if (isString(padding)) {
+    return padding + value
+  }
+  throw new Error(`Expected string or number, got ${padding}`)
+}
+
+function padLeft2(value: string, padding: string | number) {
+  if (typeof padding === 'number') {
+    return Array(padding + 1).join(' ') + value
+  }
+  if (typeof padding === 'string') {
+    return padding + value
+  }
+  throw new Error(`Expected string or number, got ${padding}`)
+}
+
 export { }
