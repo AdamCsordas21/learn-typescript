7be1a5a0a8402f27efc83b19cfb79ac8e351be7a
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-10 18:13:06 +0100

    add instanceof Type Guards example

--- src/advanced-types.ts
+++ src/advanced-types.ts
@@ -85,4 +85,42 @@ function padLeft2(value: string, padding: string | number) {
   throw new Error(`Expected string or number, got ${padding}`)
 }
 
+// instanceof Type Guards
+interface Padder {
+  getPaddingString(): string
+}
+
+class SpaceRepeatingPadder implements Padder {
+  constructor(private numSpaces: number) {}
+  getPaddingString() {
+    return Array(this.numSpaces + 1).join('')
+  }
+}
+
+class StringPadder implements Padder {
+  constructor(private value: string) {}
+  getPaddingString() {
+    return this.value
+  }
+}
+
+function getRandomPadder() {
+  return Math.random() < 0.5
+  ? new SpaceRepeatingPadder(4)
+  : new StringPadder('  ')
+}
+
+let padder: Padder = getRandomPadder()
+//        ^ = let padder: Padder
+
+if (padder instanceof SpaceRepeatingPadder) {
+  padder
+//      ^ = Could not get LSP result: er;>
+}
+if (padder instanceof StringPadder) {
+  padder
+//      ^ = Could not get LSP result: er;>
+}
+
+
 export { }
