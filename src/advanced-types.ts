871fe90104b8968f9407347616d6f5bdc7914a1c
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-13 20:31:03 +0100

    add Polymorphic this Type example

--- src/advanced-types.ts
+++ src/advanced-types.ts
@@ -268,4 +268,37 @@ bear.honey
 // Much of the time when talk about 'singleton types', we're referring to both enum member types as well as
 // numeric/string literal types, though many users will use 'singleton types' and 'literal types' interchangeably.
 
+// Polymorphic this Type
+class BasicCalculator {
+  public constructor(protected value: number = 0) {}
+  public currentValue(): number {
+    return this.value
+  }
+  public add(operand: number): this {
+    this.value += operand
+    return this
+  }
+  public multiply(operand: number): this {
+    this.value *= operand
+    return this
+  }
+}
+
+let v = new BasicCalculator(2).multiply(5).add(1).currentValue()
+
+class ScientificCalculator extends BasicCalculator {
+  public constructor(value = 0) {
+    super(value)
+  }
+  public sin() {
+    this.value = Math.sin(this.value)
+    return this
+  }
+}
+
+let v2 = new ScientificCalculator(2).multiply(5).sin().add(1).currentValue
+// Without this type, ScientificCalculator would not have been able to extend BasicCalculator and keep the fluent
+// interface. multiply would have returned BasicCalculator, which doens't have the sin method. However, this types,
+// multiply return this, which is ScientificCalculator here.
+
 export { }
