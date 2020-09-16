d8dfc7338971fa3ed9a2debec588fc5e989eaa96
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-17 00:04:52 +0100

    add Exclude<Type, ExcludeUnion> example

--- src/utility-types.ts
+++ src/utility-types.ts
@@ -93,4 +93,10 @@ const todo5: TodoPreview2 = {
 
 todo5
 
+// Exclude<Type, ExcludeUnion>
+// Constructs a type by excluding from Type all union members that are assignable to ExcludedUnion.
+type T0 = Exclude<'a' | 'b' | 'c', 'a'>
+type T1 = Exclude<'a' | 'b' | 'c', 'a' | 'b'>
+type T2 = Exclude<string | number | (() => void), Function>
+
 export {}
