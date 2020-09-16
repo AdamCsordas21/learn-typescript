31a3e5afc65e942d294ad5393a36e7d6ed0e0280
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-16 23:49:37 +0100

    add Readonly<Type> example

--- src/utility-types.ts
+++ src/utility-types.ts
@@ -22,4 +22,24 @@ const todo2 = updateTodo(todo1, {
   description: 'throw out trash'
 })
 
+// Readonly<Type
+// Constructs a type all properties of Type set to readonly, meaning the properties of the constructed type cannot be
+// reassigned.
+
+interface Todo2 {
+  title: string
+}
+
+const todo3: Readonly<Todo2> = {
+  title: 'Delete inactive users'
+}
+
+// todo3.title = 'Hello'
+// Cannot assign to 'title' because it is a read-only property.ts(2540)
+
+// This utility is useful for representing assignment expression that will fail at runtime (i.e. when attempting to
+// reassign properties of a frozen object).
+// Object.freeze
+// function freeze<Type>(obj: Type): Readonly<Type>
+
 export {}
