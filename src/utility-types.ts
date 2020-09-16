c6712a6f7fdba72b75c1829a1de846d912a94c9f
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-17 00:14:53 +0100

    add Parameters<Type> example

--- src/utility-types.ts
+++ src/utility-types.ts
@@ -109,4 +109,22 @@ type T4 = Extract<string | number | (() => void), Function>
 type T5 = NonNullable<string | number | undefined>
 type T6 = NonNullable<string[] | null | undefined>
 
+// Parameters<Type>
+// Constructs a tuple type from the types used in the parameters of a function type Type.
+declare function f1(arg: { a: number, b: string }): void
+
+type T7 = Parameters<() => string>
+type T8 = Parameters<(s: string) => void>
+type T9 = Parameters<<T>(arg: T) => T>
+type T10 = Parameters<typeof f1>
+type T11 = Parameters<any>
+type T12 = Parameters<never>
+type T13 = Parameters<never>
+// type T14 = Parameters<string>
+// Type 'string' does not satisfy the constraint '(...args: any) => any'.ts(2344)
+
+// type T15 = Parameters<Function>
+// Type 'Function' does not satisfy the constraint '(...args: any) => any'.
+// Type 'Function' provides no match for the signature '(...args: any): any'.ts(2344)
+
 export {}
